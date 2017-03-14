45a78e6ff483494e1770259ca52d6e8584b11302
Wenxin Hu <hu@th-brandenburg.de>
2017-03-14 14:17:38 +0100

    current version

--- src/main/java/org/camunda/bpm/edugraph/extraction/EduGraphApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.edugraph.extraction;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Edu Graph App")
-public class EduGraphApplication extends ServletProcessApplication {
-	// empty implementation
-}
